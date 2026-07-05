f64c370aac398f3b0ce5d9fccf5deb0150fec8f0
Patrick Vink <patrickvink@hotmail.com>
2026-07-05 21:00:42 +0200

    "Paleo Chocolate Covered Caramels"

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -4,11 +4,12 @@ import { data } from '../utils/data';
 export const RecipeListPage = () => {
     // You can play around with the console log,
     // but ultimately remove it once you are done
-    console.log(data.hits[0].recipe.label);
+    const recipe = data.hits[0].recipe;
+    console.log(recipe.label);
 
     return (
         <Center h="100vh" flexDir="column">
-            <Heading>Your Recipe App</Heading>
+            <Heading>{recipe.label}</Heading>
         </Center>
     );
 };
